09f4fd09a8fb6c7da41db4e822b4eb75bf5d113f
Enegriy <nei86@mail.ru>
2015-02-24 11:12:11 +0300

    Добавлен скрипт delete_data_package для удаления дата пакетов перед диплоем

---
+++ delete_data_package.pl
@@ -0,0 +1,17 @@
+my $dir = "D:\\Salary\\Source\\Server\\packs";
+
+opendir(DIR, $dir) or die "can't opendir $dir: $!";
+
+$count_files_was_delete = 0;
+while (defined(my $file = readdir(DIR))) {
+	if($file =~ /Data[2]?.pkg/i)
+	{
+		unlink($dir."\\".$file) or warn "failed on $dir\\$file: $!\n";
+		print $file."\n";
+		$count_files_was_delete++;
+	}
+}
+
+print "Delete count Files: $count_files_was_delete";
+
+closedir(DIR);
